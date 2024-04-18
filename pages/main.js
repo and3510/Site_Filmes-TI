edf7ce5a58b194e0e43eac4930f84ee50a4642f5
anderson <and.dovale@gmail.com>
2024-04-18 00:27:24 -0300

    finish home and form

--- pages/main.js
+++ pages/main.js
@@ -25,4 +25,18 @@ likeButtons.forEach(botaoCurtir => {
     });
 });
 
+const toggleThemeButton = document.getElementById('toggleThemeButton');
+const bodyElement = document.body;
+
+let currentTheme = 'light'; // Tema inicial definido como claro
+
+toggleThemeButton.addEventListener('click', function() {
+    if (currentTheme === 'light') {
+        currentTheme = 'dark';
+        bodyElement.classList.add('dark-mode');
+    } else {
+        currentTheme = 'light';
+        bodyElement.classList.remove('dark-mode');
+    }
+});
 
